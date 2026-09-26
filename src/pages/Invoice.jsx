785878c216a9fbd2cbb749c840e35904ce57fbbd
Huligesh Malka <huligeshmalka@gmail.com>
2026-09-26 21:35:37 +0530

    Add vercel.json

--- src/pages/Invoice.jsx
+++ src/pages/Invoice.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import { useEffect, useState } from "react";
 import { useParams, Link } from "react-router-dom";
 import API from "../services/api";
 import {
@@ -1121,4 +1120,3 @@ styleSheet.textContent = `
     }
 `;
 document.head.appendChild(styleSheet);
-
